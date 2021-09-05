a282674f8b94c1b8c7826086076ca63c03fc6470
haujin <uj0110@g.skku.edu>
2021-09-05 22:20:49 +0900

    파일

--- JavaScript_DreamCoding/async/callback.js
+++ JavaScript_DreamCoding/async/callback.js
@@ -2,6 +2,7 @@
 
 //JavqScript is synchronous
 console.log('1');
+//setTimeout(Timehandler라는 콜백함수지정, 얼마동안의 시간)
 setTimeout(() => console.log('2'), 1000); //지금 당장 실행하진 않고 나중에 부르는 것
 console.log('3');
 
@@ -16,3 +17,5 @@ function printWithDelay(print, timeout){
     setTimeout(print, timeout);
 }
 printWithDelay(() => console.log('async callback'), 2000);
+
+throw new Error()
